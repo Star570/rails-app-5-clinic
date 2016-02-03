4fd6f1b9b86032530855fad94ae5d6ba223e3355
CY Chen <cychen0429@gmail.com>
2016-02-03 23:20:48 +0800

    add some css

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+  def font_bold_class(link_path)
+    link_controller = Rails.application.routes.recognize_path(link_path)[:controller]
+    link_id = Rails.application.routes.recognize_path(link_path)[:id]
+    current_controller = params[:controller]
+    current_id = params[:id]
+    if (link_controller == current_controller) && (link_id == current_id)
+      "font-weight: bold"
+    else
+      ""
+    end
+  end
+
 end
