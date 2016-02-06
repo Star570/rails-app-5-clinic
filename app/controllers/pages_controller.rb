e2c470e3abffee0fecc50df2275d268a24670d36
CY Chen <cychen0429@gmail.com>
2016-02-06 19:11:37 +0800

    add flash notice

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,19 +1,23 @@
 class PagesController < ApplicationController
   def announcements
+    flash[:notice] = "Hello"       
     @announcements = Announcement.all.order("created_at DESC")    
   end
 
   def column
+    flash[:notice] = "Hello"        
     @posts = Post.all.order("created_at DESC")
     @categories = Category.all.order("created_at ASC")
   end
 
   def message_board
+    flash[:notice] = "Hello"        
     @messages = Message.all.order("created_at DESC")    
     @message = Message.new    
   end
 
   def booking
+    flash[:notice] = "Hello"        
     BookingDate.update_latest_booking
     @booking_dates = BookingDate.all
   end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,23 @@
 module ApplicationHelper
 
+  def notice_message
+    alert_types = { notice: :success, alert: :danger }
+
+    close_button_options = { class: "close", "data-dismiss" => "alert", "aria-hidden" => true }
+    close_button = content_tag(:button, "×", close_button_options)
+
+    alerts = flash.map do |type, message|
+      alert_content = close_button + message
+
+      alert_type = alert_types[type.to_sym] || type
+      alert_class = "alert alert-#{alert_type} alert-dismissable"
+
+      content_tag(:div, alert_content, class: alert_class)
+    end
+
+    alerts.join("\n").html_safe
+  end  
+  
   def font_bold_class(link_path)
     link_controller = Rails.application.routes.recognize_path(link_path)[:controller]
     link_id = Rails.application.routes.recognize_path(link_path)[:id]
