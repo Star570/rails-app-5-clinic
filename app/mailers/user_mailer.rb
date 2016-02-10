c2c32940f50fb6e8c66bb5226de108e960227c30
CY Chen <cychen0429@gmail.com>
2016-02-10 19:11:44 +0800

    modify mail title

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,21 +5,21 @@ class UserMailer < ApplicationMailer
   def notify_pin(user)
     @user = user
     mail(:to => @user.email, 
-         :subject => "歡迎到CY診所註冊")
+         :subject => "歡迎到[CY診所]註冊")
   end  
 
   def notify_add_reservation(user, reservation)
     @user = user
     @reservation = reservation
     mail(:to => @user.email, 
-         :subject => "[CY診所]您的預約記錄")
+         :subject => "[CY診所] 您的預約記錄")
   end  
 
   def notify_cancel_reservation(user, reservation)
     @user = user
     @reservation = reservation
     mail(:to => @user.email, 
-         :subject => "[CY診所]您的預約記錄已取消")
+         :subject => "[CY診所] 您的預約記錄已取消")
   end  
 
 end
