31683a30c8a22573848e8dcedd17afe33d6b4a2d
CY Chen <cychen0429@gmail.com>
2016-03-12 20:00:31 +0800

    add schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,25 +1,11 @@
-# Use this file to easily define all of your cron jobs.
-#
-# It's helpful, but not entirely necessary to understand cron before proceeding.
-# http://en.wikipedia.org/wiki/Cron
+require "tzinfo"
 
-# Example:
-#
-# set :output, "/path/to/my/cron_log.log"
-#
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
-
-# Learn more: http://github.com/javan/whenever
 set :output, 'log/cron.log'
 
-every 1.day, :at => Time.zone.parse('20:00 pm').utc do
+def local(time)
+  TZInfo::Timezone.get("Taipei").local_to_utc(Time.parse(time))
+end
+
+every 1.day, :at => at: local("8:01 pm") do
   rake "slot:generate"
 end
