57eaf98e9a8e858933f24361131c1837398206c3
CY Chen <cychen0429@gmail.com>
2016-03-13 15:51:47 +0800

    test

--- app/controllers/announcement_photos_controller.rb
+++ app/controllers/announcement_photos_controller.rb
@@ -10,12 +10,14 @@ class AnnouncementPhotosController < ApplicationController
       
       if @photo.save
         p "photo save成功"
-        p @photo.image.url          
+        p @photo.image.url       
         success=true     
         render json: { :success=> success, :msg=>msg, :file_path=> @photo.image.url }
       else
         p "photo save失敗"
-        p @photo.image.url            
+        p @photo.image.url   
+        Rails.logger.info(@your_object.errors.inspect)    
+        p Rails.logger.info(@your_object.errors.inspect)       
         success=false
         render json: { :success=> false }
       end
